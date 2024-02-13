c6795c0c159f97d412f713f04488404d76c76546
EmersonTeixeira1 <emerson.teixeira@atmodigital.com.br>
2024-02-13 18:00:14 -0300

    Criação do padrão PSR-4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c083357318313e5888b0ec9d692feafdf6499c46',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c083357318313e5888b0ec9d692feafdf6499c46',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
